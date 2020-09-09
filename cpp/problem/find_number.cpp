0ced6f904a764822c5884a76251330f9cddbdd7c
xinantrash <1198629131@qq.com>
2020-09-09 18:02:20 +0800

    09-09

--- cpp/problem/find_number.cpp
+++ cpp/problem/find_number.cpp
@@ -42,3 +42,50 @@ int main(int argc, const char *argv[]) {
 
 	return 0;
 }
+
+// #include <bits/stdc++.h>
+// 
+// using namespace std;
+// 
+// int nums[1000000] = {0};
+// int n;
+// int m;
+// int k;
+// 
+// int find_number(int start, int end, int nums[], int k) {
+// 	if ( start == end ) {
+// 		if ( nums[start] == k ) {
+// 			return start+1;
+// 		}
+// 
+// 		else {
+// 			return -1;
+// 		}
+// 	}
+// 
+// 	int mid = (start+end) / 2;
+// 
+// 	if ( k <= nums[mid] ) {
+// 		find_number(start, mid, nums, k);
+// 	}
+// 	else {
+// 		find_number(mid+1, end, nums, k);
+// 	}
+// }
+// 
+// int main(int argc, const char *argv[]) {
+// 
+// 	cin >> n >> m;
+// 
+// 	for ( int i=0; i<n; i++ ) {
+// 		cin >> nums[i];
+// 	}
+// 
+// 
+// 	for ( int i=1; i<=m; i++ ) {
+// 		cin >> k;
+// 		cout << find_number(0, n-1, nums, k) << " ";
+// 	}
+// 
+// 	return 0;
+// }
